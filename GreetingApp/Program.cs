6e84f1f97b3bda15140a0d961354c69db9f308d6
EktaMall <mallekta541@gmail.com>
2026-01-12 15:38:19 +0530

    Greeting Library

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreetingLibrary;
+
+namespace GreetingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+
+            string message = GreetingHelper.GetGreeting(name);
+
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+        }
+    }
+}
