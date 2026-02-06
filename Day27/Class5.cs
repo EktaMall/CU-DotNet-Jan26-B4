50d8008d2a71743738751641cdea118683a2ac21
EktaMall <mallekta541@gmail.com>
2026-02-06 11:49:41 +0530

    Day27 Uploaded

---
+++ Day27/Class5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDailyLogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = @"..\..\..\";
+
+            if (!Directory.Exists(dir))
+            {
+                Console.WriteLine("Directory doesn't exist");
+                return;
+            }
+            string file = "journal.txt";
+            string path = dir + file;
+
+            Console.WriteLine("DAILY REFLECTION");
+            string data = Console.ReadLine();
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine(data);
+            }
+        }
+    }
+}
