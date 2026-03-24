106cfc5477396f59682083de2380d3dceeab2447
EktaMall <mallekta541@gmail.com>
2026-03-24 11:55:35 +0530

    Done

---
+++ Assessments/Week11/MVC_DI/MVC_DI/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_DI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string PromoCode { get; set; }
+        public decimal DiscountedPrice { get; set; }
+    }
+}
---
+++ Assessments/Week11/MVC_DI/MVC_DI/Services/IPricingService.cs
@@ -0,0 +1,8 @@
+﻿namespace MVC_DI.Services
+{
+
+    public interface IPricingService
+    {
+        decimal CalculateFinalPrice(decimal basePrice, string promoCode);
+    }
+}
